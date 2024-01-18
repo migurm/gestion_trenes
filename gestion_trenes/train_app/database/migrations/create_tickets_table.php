94322a20068e2d8f579a34b09b4d83a42cb715dd
Miguel Angel Rueda Moreno <migue.rueda.90@gmail.com>
2024-01-18 18:26:49 +0100

    .env & migrations (tables)

--- gestion_trenes/train_app/database/migrations/create_tickets_table.php
+++ gestion_trenes/train_app/database/migrations/create_tickets_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
+            $table->date('date');
+            $table->float('price');
+
+            $table->unsignedBigInteger('train_id');
+            $table->foreign('train_id')->references('id')->on('trains');
+
+            $table->unsignedBigInteger('ticket_type_id');
+            $table->foreign('ticket_type_id')->references('id')->on('ticket_types');
+
             $table->timestamps();
         });
     }
--- gestion_trenes/train_app/database/migrations/create_trains_table.php
+++ gestion_trenes/train_app/database/migrations/create_trains_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('passengers');
+            $table->year('year');
+            $table->unsignedBigInteger('train_type_id');
+            //It's a foreign key
+            $table->foreign('train_type_id')->references('id')->on('train_types');
             $table->timestamps();
         });
     }
